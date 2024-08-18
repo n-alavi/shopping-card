4216b8e6f82655e6f90ad141bc609fde814e1534
nargess alavi <alavi_n70@yahoo.com>
2024-08-19 02:44:31 +0330

    create navbar component

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className="h-9 border-b shadow flex justify-between flex-row-reverse items-center ">
+      <ul className="flex gap-4">
+        <li>
+          <Link to="/">خانه</Link>
+        </li>
+        <li>
+          <Link to="/store">فروشگاه</Link>
+        </li>
+      </ul>
+      <div>
+        <button>سبد خرید</button>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
