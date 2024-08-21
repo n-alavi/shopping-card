353398751bedcfc5bb62fe6d4168294a61974daa
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 02:18:15 +0330

    create layout component ant replace the routes to layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Store from "./Pages/Store/Store";
-import Navbar from "./Components/Navbar/navbar/Navbar";
+import Layout from "./Components/layout/Layout";
 
 function App() {
   return (
-    <>
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
       </Routes>
-    </>
+    </Layout>
   );
 }
 
--- src/Components/layout/Layout.tsx
+++ src/Components/layout/Layout.tsx
@@ -1,14 +1,12 @@
 import Navbar from "../Navbar/navbar/Navbar";
 
-interface Layout {
-  children: React.ReactNode;
+function Layout({ children }: { children: React.ReactNode }): JSX.Element {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
 
-function Layout({ children }: Layout) {
-  return;
-  <>
-    <Navbar />
-    {children}
-  </>;
-}
 export default Layout;
