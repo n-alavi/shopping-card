404a57378c039688ab75d34fd6898a999ba3dc2d
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 03:16:54 +0330

    create productitem component and add it on the store

---
+++ src/Components/productitem/ProductItem.tsx
@@ -0,0 +1,23 @@
+function ProductItem() {
+  return (
+    <div className="shadow border rounded">
+      <img
+        className="rounded"
+        src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Laptop-Go-3-001-3000x1682:VP2-859x540"
+      />
+      <div className="flex flex-row-reverse justify-between p-4">
+        <h3>عنوان محصول</h3>
+        <span>55$</span>
+      </div>
+      <div className="p-2">
+        <p className="line-clamp-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error sint
+          cum laborum harum qui nam tempore! Dolor consequatur eaque, fugit
+          adipisci nostrum eveniet! Voluptatum magnam pariatur ut nesciunt!
+          Natus, cupiditate?
+        </p>
+      </div>
+    </div>
+  );
+}
+export default ProductItem;
--- src/Pages/Store/Store.tsx
+++ src/Pages/Store/Store.tsx
@@ -1,4 +1,22 @@
+import Container from "../../Components/Navbar/container/Container";
+import ProductItem from "../../Components/productitem/ProductItem";
+
 function Store() {
-  return <>store page</>;
+  return (
+    <>
+      <h1 className="text-right mt-4 mx-4">جدیدترین محصولات</h1>
+      <Container>
+        <div className="grid grid-cols-4 gap-4 mt-4">
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+        </div>
+      </Container>
+    </>
+  );
 }
 export default Store;
