6b0a460dce2d18b7e09c1eb2824fd28f5a248293
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 01:47:39 +0330

    add px in container component & add container to navbar

--- src/Components/Navbar/container/Container.tsx
+++ src/Components/Navbar/container/Container.tsx
@@ -2,6 +2,6 @@ interface Container {
   children: React.ReactNode;
 }
 function Container({ children }: Container) {
-  return <div className="container mx-auto">{children}</div>;
+  return <div className="container mx-auto px-32">{children}</div>;
 }
 export default Container;
--- src/Components/Navbar/navbar/Navbar.tsx
+++ src/Components/Navbar/navbar/Navbar.tsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
+import Container from "../container/Container";
 
 function Navbar() {
   return (
-    <div className="h-9 border-b shadow flex justify-between flex-row-reverse items-center ">
-      <ul className="flex gap-4">
-        <li>
-          <Link to="/">خانه</Link>
-        </li>
-        <li>
-          <Link to="/store">فروشگاه</Link>
-        </li>
-      </ul>
-      <div>
-        <button>سبد خرید</button>
-      </div>
+    <div className="h-9 border-b shadow flex items-center ">
+      <Container>
+        <div className="flex justify-between flex-row-reverse ">
+          <ul className="flex gap-4">
+            <li>
+              <Link to="/">خانه</Link>
+            </li>
+            <li>
+              <Link to="/store">فروشگاه</Link>
+            </li>
+          </ul>
+          <div>
+            <button>سبد خرید</button>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
