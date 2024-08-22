f1e794f6dd2bed413e79ff2b04c62fc66468441b
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 23:29:39 +0330

    complete the button component and create the condition

--- src/Components/button/Button.tsx
+++ src/Components/button/Button.tsx
@@ -1,8 +1,31 @@
-type TButton = {
-  children: React.ReactNode;
+import { ComponentProps } from "react";
+
+type Tvariant = "primary" | "secondary" | "danger" | "warning" | "success";
+type TButton = ComponentProps<"button"> & {
+  variant: Tvariant;
 };
 
-function Button({ children }: TButton) {
-  return <button>{children}</button>;
+function Button({ children, variant, ...rest }: TButton) {
+  console.log(checkvariant(variant));
+
+  return (
+    <button {...rest} style={{ ...checkvariant(variant) }}>
+      {children}
+    </button>
+  );
 }
 export default Button;
+
+function checkvariant(variant: Tvariant) {
+  if (variant === "primary") {
+    return { backgroundColor: "blue", color: "white" };
+  } else if (variant === "secondary") {
+    return { backgroundColor: "gray", color: "black" };
+  } else if (variant === "danger") {
+    return { backgroundColor: "red", color: "black" };
+  } else if (variant === "success") {
+    return { backgroundColor: "green", color: "black" };
+  } else if (variant === "warning") {
+    return { backgroundColor: "yellow", color: "black" };
+  }
+}
