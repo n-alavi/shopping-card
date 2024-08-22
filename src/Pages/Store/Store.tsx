1bfa38c1367555ad1d11000d8e878e1c010cdaff
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 13:47:33 +0330

    link the product pages to store

--- src/Pages/Store/Store.tsx
+++ src/Pages/Store/Store.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Container from "../../Components/Navbar/container/Container";
 import ProductItem from "../../Components/productitem/ProductItem";
 
@@ -7,13 +8,21 @@ function Store() {
       <h1 className="text-right mt-4 mx-4">جدیدترین محصولات</h1>
       <Container>
         <div className="grid grid-cols-4 gap-4 mt-4">
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
+          <Link to={`product/${1}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`product/${2}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`product/${3}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`product/${4}`}>
+            <ProductItem />
+          </Link>
+          <Link to={`product/${5}`}>
+            <ProductItem />
+          </Link>
         </div>
       </Container>
     </>
