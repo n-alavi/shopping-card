701a558ece45856470f4967cafde2d784f76386b
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 15:11:20 +0330

    add some ui to this component

--- src/Pages/productpage/ProductPage.tsx
+++ src/Pages/productpage/ProductPage.tsx
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
+import Container from "../../Components/Navbar/container/Container";
 
 function ProductPage() {
   const params = useParams();
+
   return (
     <div>
-      <h1 className="text-9xl">{params.id}</h1>
+      <Container>
+        <div className=" h-96 shadow mt-4 grid grid-cols-12">
+          <div className="col-span-10 bg-slate-900"></div>
+          <div className="bg-red-300 col-span-2 p-4">
+            <img
+              className="rounded"
+              src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Laptop-Go-3-001-3000x1682:VP2-859x540"
+            />
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
