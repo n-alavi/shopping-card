f9230d80fede5f0ce01060796fcce2b39efc9232
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 15:10:46 +0330

    add link to productpage

--- src/Pages/Store/Store.tsx
+++ src/Pages/Store/Store.tsx
@@ -8,19 +8,19 @@ function Store() {
       <h1 className="text-right mt-4 mx-4">جدیدترین محصولات</h1>
       <Container>
         <div className="grid grid-cols-4 gap-4 mt-4">
-          <Link to={`product/${1}`}>
+          <Link to={`/product/${1}`}>
             <ProductItem />
           </Link>
-          <Link to={`product/${2}`}>
+          <Link to={`/product/${2}`}>
             <ProductItem />
           </Link>
-          <Link to={`product/${3}`}>
+          <Link to={`/product/${3}`}>
             <ProductItem />
           </Link>
-          <Link to={`product/${4}`}>
+          <Link to={`/product/${4}`}>
             <ProductItem />
           </Link>
-          <Link to={`product/${5}`}>
+          <Link to={`/product/${5}`}>
             <ProductItem />
           </Link>
         </div>
