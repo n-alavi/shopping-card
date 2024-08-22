d38b1de49adc115e115c54f554fc82017b2307a8
nargess alavi <alavi_n70@yahoo.com>
2024-08-22 15:40:31 +0330

    compelete ui productpage component

--- src/Pages/productpage/ProductPage.tsx
+++ src/Pages/productpage/ProductPage.tsx
@@ -7,13 +7,29 @@ function ProductPage() {
   return (
     <div>
       <Container>
-        <div className=" h-96 shadow mt-4 grid grid-cols-12">
-          <div className="col-span-10 bg-slate-900"></div>
-          <div className="bg-red-300 col-span-2 p-4">
+        <div className="h-96 shadow mt-4 grid grid-cols-12">
+          <div className="col-span-10  p-6">
+            <h1 className="text-right">عنوان محصول</h1>
+            <div>
+              <p className="text-right">قیمت: 25 تومان</p>
+              <p className="text-right">
+                البته مهم‌ترین مزیت لپ‌تاپ و درواقع وجه تمایز آن با رایانه‌های
+                رومیزی این است که شما سر کار، حین پرواز، یا وقتی در کافه
+                نشسته‌اید و قهوه نوش جان می‌کنید. در تمام این موارد می‌توانید از
+                لپ‌تاپ استفاده کنید. اما گذشته از این مورد شاید بتوان به مواردی
+                دیگر نیز اشاره کرد: کوچک و قابل حمل بودن انعطاف پذیری صفحه کلید
+                با قابلیت تاشو بودن سروصدای کمتر در هنگام کار کردن
+              </p>
+            </div>
+          </div>
+          <div className=" col-span-2 bg-sky-200 p-4">
             <img
               className="rounded"
               src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Laptop-Go-3-001-3000x1682:VP2-859x540"
             />
+            <div>
+              <button>add to cart</button>
+            </div>
           </div>
         </div>
       </Container>
