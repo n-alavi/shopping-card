a724921b9cfc0432d272987fcb4bd37aac444701
nargess alavi <alavi_n70@yahoo.com>
2024-08-23 15:47:55 +0330

    set route and import card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home";
 import Store from "./Pages/Store/Store";
 import Layout from "./Components/layout/Layout";
 import ProductPage from "./Pages/productpage/ProductPage";
+import Card from "./Pages/card/Card";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/card" element={<Card />} />
       </Routes>
     </Layout>
   );
