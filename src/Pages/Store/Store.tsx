145ffc1b2c16b2893e6076ff5e39017f2c19fd2b
nargess alavi <alavi_n70@yahoo.com>
2024-08-24 11:13:44 +0330

    use useEffect and use getproduct() for api and we use .then that we can get result(data) from function

--- src/Pages/Store/Store.tsx
+++ src/Pages/Store/Store.tsx
@@ -1,28 +1,26 @@
 import { Link } from "react-router-dom";
 import Container from "../../Components/Navbar/container/Container";
 import ProductItem from "../../Components/productitem/ProductItem";
+import { useEffect, useState } from "react";
+import { getProduct } from "../../services/api";
 
 function Store() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProduct().then((result) => {
+      setProducts(result);
+    });
+  }, []);
   return (
     <>
-      <h1 className="text-right mt-4 mx-4">جدیدترین محصولات</h1>
       <Container>
+        <h1 className="text-right mt-4 mx-4">جدیدترین محصولات</h1>
         <div className="grid grid-cols-4 gap-4 mt-4">
-          <Link to={`/product/${1}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${2}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${3}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${4}`}>
-            <ProductItem />
-          </Link>
-          <Link to={`/product/${5}`}>
-            <ProductItem />
-          </Link>
+          {products.map((item) => (
+            <Link key= to={`/product/${1}`}>
+              <ProductItem />
+            </Link>
+          ))}
         </div>
       </Container>
     </>
