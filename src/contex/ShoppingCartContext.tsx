9961105592affca35b011341623203174631943f
nargess alavi <alavi_n70@yahoo.com>
2024-08-25 02:50:02 +0330

    creat contex export it. creat a provider for contex this functional component get a children. this children are all of our route in the app.tsx . creat usestate for cartitem and set it on the value. type of our state is array & certain type of context

---
+++ src/contex/ShoppingCartContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+interface ShoppingCartProvider {
+  children: React.ReactNode;
+}
+interface ShoppingCartContext {
+  cartItems: [];
+}
+
+export const ShoppingCartContext = createContext<ShoppingCartContext>({
+  cartItems: [],
+});
+
+export function ShoppingCartProvider({ children }: ShoppingCartProvider) {
+  const [cartItems, setCartItems] = useState<[]>([]);
+
+  return (
+    <ShoppingCartContext.Provider value={{ cartItems }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
