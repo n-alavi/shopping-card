7f927e061dc121fd9c651973fd439aa7124c6d4d
nargess alavi <alavi_n70@yahoo.com>
2024-08-24 23:52:15 +0330

    add useEffect and function for get api single product

--- src/Pages/productpage/ProductPage.tsx
+++ src/Pages/productpage/ProductPage.tsx
@@ -1,33 +1,32 @@
 import { useParams } from "react-router-dom";
 import Container from "../../Components/Navbar/container/Container";
 import Button from "../../Components/button/Button";
+import { useEffect, useState } from "react";
+import { getSingleProduct } from "../../services/api";
+import { Product } from "../../types/server";
 
 function ProductPage() {
-  const params = useParams();
+  const params = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product>();
+  useEffect(() => {
+    getSingleProduct(params.id as string).then((data) => {
+      setProduct(data);
+    });
+  }, []);
 
   return (
     <div>
       <Container>
         <div className="h-96 shadow mt-4 grid grid-cols-12">
           <div className="col-span-10  p-6">
-            <h1 className="text-right">عنوان محصول</h1>
+            <h1 className="text-right">{product?.title}</h1>
             <div>
-              <p className="text-right">قیمت: 25 تومان</p>
-              <p className="text-right">
-                البته مهم‌ترین مزیت لپ‌تاپ و درواقع وجه تمایز آن با رایانه‌های
-                رومیزی این است که شما سر کار، حین پرواز، یا وقتی در کافه
-                نشسته‌اید و قهوه نوش جان می‌کنید. در تمام این موارد می‌توانید از
-                لپ‌تاپ استفاده کنید. اما گذشته از این مورد شاید بتوان به مواردی
-                دیگر نیز اشاره کرد: کوچک و قابل حمل بودن انعطاف پذیری صفحه کلید
-                با قابلیت تاشو بودن سروصدای کمتر در هنگام کار کردن
-              </p>
+              <p className="text-right">{product?.price}$</p>
+              <p className="text-right">{product?.description}</p>
             </div>
           </div>
           <div className=" col-span-2 bg-sky-200 p-4">
-            <img
-              className="rounded"
-              src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Laptop-Go-3-001-3000x1682:VP2-859x540"
-            />
+            <img className="rounded" src={product?.image} />
             <Button className="w-full mt-2" variant="primary">
               {" "}
               add to cart
