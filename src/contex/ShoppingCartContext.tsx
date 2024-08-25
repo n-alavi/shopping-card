48933edf4093ad9ae12209d5ddd45c457e09f491
nargess alavi <alavi_n70@yahoo.com>
2024-08-25 18:28:19 +0330

    certain type of cartitem & usestate

--- src/contex/ShoppingCartContext.tsx
+++ src/contex/ShoppingCartContext.tsx
@@ -3,14 +3,20 @@ import { createContext, useState } from "react";
 interface ShoppingCartProvider {
   children: React.ReactNode;
 }
+
+interface CartItem {
+  id: number;
+  qty: number;
+}
+
 interface ShoppingCartContext {
-  cartItems: [];
+  cartItems: CartItem[];
 }
 
 export const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
 export function ShoppingCartProvider({ children }: ShoppingCartProvider) {
-  const [cartItems, setCartItems] = useState<[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   return (
     <ShoppingCartContext.Provider value={{ cartItems }}>
