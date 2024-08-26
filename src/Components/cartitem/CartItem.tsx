c1a143601bf4482d639f49b61a29e281a4d3eadc
nargess alavi <alavi_n70@yahoo.com>
2024-08-26 19:11:50 +0330

    we use useeffect for get data with id from back-end. give data in the usestate product and use it for our cartitem for example {product.title}

--- src/Components/cartitem/CartItem.tsx
+++ src/Components/cartitem/CartItem.tsx
@@ -1,18 +1,25 @@
+import { useEffect, useState } from "react";
 import Button from "../button/Button";
+import { getSingleProduct } from "../../services/api";
+import { Product } from "../../types/server";
 
 interface ICartItem {
   id: number;
   qty: number;
 }
-function CartItem({}: ICartItem) {
+function CartItem({ id, qty }: ICartItem) {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    getSingleProduct(id).then((data) => {
+      setProduct(data);
+    });
+  }, []);
   return (
     <div className="flex flex-row-reverse mt-4 border-b pb-2">
-      <img
-        className="rounded w-28"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHihA-fyXMXY4nGNl0Ro6E-hX85EirVYaoVQ&s"
-      />
+      <img className="rounded w-28" src={product?.image} />
       <div className="mr-4">
-        <h3 className="text-right">عنوان محصول</h3>
+        <h3 className="text-right">{product?.title}</h3>
 
         <div className="mt-2">
           <Button className="mr-2" variant="danger">
@@ -22,7 +29,7 @@ function CartItem({}: ICartItem) {
           <Button className="mr-2" variant="primary">
             +
           </Button>
-          <span className="mr-2">{2}</span>
+          <span className="mr-2">{qty}</span>
           <Button className="mr-2" variant="primary">
             -
           </Button>
