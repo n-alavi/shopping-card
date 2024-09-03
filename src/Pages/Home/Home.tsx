15c8f4307cf8cabccd83ec5f8d347176e250579f
nargess alavi <alavi_n70@yahoo.com>
2024-09-03 21:12:56 +0330

    feat: add background image to page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,21 @@
 function Home() {
-  return <>home page</>;
+  return (
+    <>
+      <div
+        className="bg-custom bg-cover bg-center h-screen"
+        style={{
+          backgroundImage:
+            "url('https://wallpapers.com/images/featured/laptop-murjp1nk4lp1idlt.webp')",
+        }}
+      >
+        <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
+          <h1 className="text-white text-4xl font-bold">
+            Welcome to My Website
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
